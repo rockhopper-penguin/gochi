f7b298546b36c4c915b5c57cd4fc91853c1fabe3
Rockhopper-Penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-09-10 13:34:13 +0000

    メイン処理追加

--- gochi/src/components/gochi.vue
+++ gochi/src/components/gochi.vue
@@ -1,17 +1,58 @@
 <template>
-  <div>
-    <h1>Gochi</h1>
+  <div class="gochi">
+    <div>
+      <p>表示するボックス数</p>
+      <p>
+        <b-form-input
+          class="input_numbers"
+          type="number"
+          min="0"
+          v-model.number="numberOfBoxes"
+          @change="deleteElement"
+        />
+      </p>
+    </div>
+    <div class="input_namaes" v-for="item in numberOfBoxes" :key="item">
+      <b-form-input type="text" v-model="saveNames[item - 1]" />
+    </div>
+    <div class="m-2">
+      <p>
+        <b-button variant="outline-secondary" @click="gochi">GOSHI!</b-button>
+      </p>
+      <p class="lead p-3">
+        {{ item }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class Gochi extends Vue {}
+export default class Gochi extends Vue {
+  private numberOfBoxes = 0;
+  private saveNames: string[] = [];
+  private item = "";
+  private gochi() {
+    this.item = this.saveNames[
+      Math.floor(Math.random() * this.saveNames.length - 0) + 0
+    ];
+  }
+  deleteElement() {
+    if (this.saveNames.length > this.numberOfBoxes) {
+      this.saveNames.splice(this.numberOfBoxes, this.saveNames.length);
+    }
+  }
+}
 </script>
 
 <style>
-h1 {
-  text-align: center;
+.gochi {
+  width: 80%;
+  margin: auto;
+  padding: 20px;
+}
+.input_namaes {
+  padding: 5px;
 }
 </style>
